683caaa928374d055bbe9d62eb11ecb9fd0d1af1
duchao <631475954@qq.com>
2016-12-01 18:01:45 +0800

    添加后台自动更新功能

--- src/com/example/coolweather/app/acticity/WeatherActivity.java
+++ src/com/example/coolweather/app/acticity/WeatherActivity.java
@@ -1,20 +1,26 @@
 package com.example.coolweather.app.acticity;
 
+import com.example.coolweather.app.service.AutoUpdateService;
 import com.example.coolweather.app.util.HttpCallbackListener;
 import com.example.coolweather.app.util.HttpUtil;
 import com.example.coolweather.app.util.Utility;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class WeatherActivity extends Activity {
+public class WeatherActivity extends Activity implements OnClickListener {
 	private LinearLayout weatherInfoLayout;
 	/**
 	 * 用于显示城市名
@@ -40,14 +46,14 @@ public class WeatherActivity extends Activity {
 	 * 用于显示当前日期
 	 */
 	private TextView currentDateText;
-	// /**
-	// * 切换城市按钮
-	// */
-	// private Button switchCity;
-	// /**
-	// * 更新天气按钮
-	// */
-	// private Button refreshWeather;
+	/**
+	 * 切换城市按钮
+	 */
+	private Button switchCity;
+	/**
+	 * 更新天气按钮
+	 */
+	private Button refreshWeather;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -62,8 +68,8 @@ public class WeatherActivity extends Activity {
 		temp1Text = (TextView) findViewById(R.id.temp1);
 		temp2Text = (TextView) findViewById(R.id.temp2);
 		currentDateText = (TextView) findViewById(R.id.current_date);
-		// switchCity = (Button) findViewById(R.id.switch_city);
-		// refreshWeather = (Button) findViewById(R.id.refresh_weather);
+		switchCity = (Button) findViewById(R.id.switch_city);
+		refreshWeather = (Button) findViewById(R.id.refresh_weather);
 		String countyCode = getIntent().getStringExtra("county_code");
 		if (!TextUtils.isEmpty(countyCode)) {
 			// 有县级代号时就去查询天气
@@ -75,31 +81,9 @@ public class WeatherActivity extends Activity {
 			// 没有县级代号时就直接显示本地天气
 			showWeather();
 		}
-//		switchCity.setOnClickListener(this);
-//		refreshWeather.setOnClickListener(this);
+		switchCity.setOnClickListener(this);
+		refreshWeather.setOnClickListener(this);
 	}
-
-//	@Override
-//	public void onClick(View v) {
-//		switch (v.getId()) {
-//		case R.id.switch_city:
-//			Intent intent = new Intent(this, ChooseAreaActivity.class);
-//			intent.putExtra("from_weather_activity", true);
-//			startActivity(intent);
-//			finish();
-//			break;
-//		case R.id.refresh_weather:
-//			publishText.setText("同步中...");
-//			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-//			String weatherCode = prefs.getString("weather_code", "");
-//			if (!TextUtils.isEmpty(weatherCode)) {
-//				queryWeatherInfo(weatherCode);
-//			}
-//			break;
-//		default:
-//			break;
-//		}
-//	}
 
 	/**
 	 * 查询县级代号所对应的天气代号。
@@ -121,6 +105,7 @@ public class WeatherActivity extends Activity {
 	 * 根据传入的地址和类型去向服务器查询天气代号或者天气信息。
 	 */
 	private void queryFromServer(final String address, final String type) {
+		showProgressDialog();
 		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
 			@Override
 			public void onFinish(final String response) {
@@ -139,6 +124,7 @@ public class WeatherActivity extends Activity {
 					runOnUiThread(new Runnable() {
 						@Override
 						public void run() {
+							closeProgressDialog();
 							showWeather();
 						}
 					});
@@ -150,6 +136,7 @@ public class WeatherActivity extends Activity {
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
+						closeProgressDialog();
 						publishText.setText("同步失败");
 					}
 				});
@@ -170,5 +157,53 @@ public class WeatherActivity extends Activity {
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.switch_city:
+			Intent intent = new Intent(this, ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity", true);
+			startActivity(intent);
+			finish();
+			break;
+		case R.id.refresh_weather:
+			publishText.setText(" 同步中...");
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			String weatherCode = prefs.getString("weather_code", "");
+			if (!TextUtils.isEmpty(weatherCode)) {
+				queryWeatherInfo(weatherCode);
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+	private ProgressDialog progressDialog;
+
+	/**
+	 * 显示进度对话框
+	 */
+	private void showProgressDialog() {
+		if (progressDialog == null) {
+			progressDialog = new ProgressDialog(this);
+			progressDialog.setMessage("正在加载...");
+			progressDialog.setCanceledOnTouchOutside(false);
+		}
+		progressDialog.show();
+	}
+
+	/**
+	 * 关闭进度对话框
+	 */
+	private void closeProgressDialog() {
+		if (progressDialog != null) {
+			progressDialog.dismiss();
+		}
+	}
+
 }
---
+++ src/com/example/coolweather/app/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,19 @@
+package com.example.coolweather.app.receiver;
+
+import com.example.coolweather.app.service.AutoUpdateService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+		Intent i = new Intent(arg0, AutoUpdateService.class);
+		arg0.startService(i);
+	}
+
+}
